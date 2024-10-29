5228d5732e92ba38cec84e4873df352ef9e83141
Ali <raza.ali5@hotmail.com>
2024-10-29 10:48:53 +0500

    local.settings.json host port

--- frontend/main.js
+++ frontend/main.js
@@ -3,17 +3,18 @@ window.addEventListener ('DOMContentLoaded',(event)=> {
     getVisitCount(); 
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeTrigger';
+const functionApiURL = 'https://aliraza-azureresume.azurewebsites.net/api/GetResumeTrigger?code=4_xwScj9ASkqYtvTwjv6hk4fJklU42PNQEMzQeftmc_GAzFu9WCKSQ%3D%3D';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeTrigger';
 
 const getVisitCount = () => {
 
     let count =30; 
-    fetch(functionApi).then(Response => {
+    fetch(functionApiURL).then(Response => {
         return Response.json()
 
     }).then(Response => {
         console.log("Website called function API")
-        count = Response.count;
+        count = Response.count; 
         document.getElementById("counter").innerText = count;
 
     }).catch(function(error){
